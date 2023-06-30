c6aa4e02536436eba358c7f02a3945b41d6fa41f
CrochetFlorent <crochet.florent@gmail.com>
2023-06-30 08:34:43 +0200

    Update

--- Jalon_algo_niveau1_fcrochet/Jalon_algo1/src/Jalon1/App.java
+++ Jalon_algo_niveau1_fcrochet/Jalon_algo1/src/Jalon1/App.java
@@ -5,28 +5,30 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-
+		// Déclaration des variables
 		String Nom;
 		String Prénom;
 		int Département;
 		String Adresse1;
 		String Adresse2;
 		
-		
+		// Création et ouverture du scanner
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Quel est votre nom?");
-		Nom = sc.next();
-		System.out.println(" Quel est votre prénom?");
-		Prénom = sc.next();
-		System.out.println("Quel est votre département?");
-		Département = sc.nextInt();
 		
-		Adresse1 = Nom+Département+Prénom+"@arfp.asso.fr";
-		Adresse2 = Prénom+"."+Nom+Département+"@arfp.asso.fr";
+		System.out.println("Quel est votre nom?"); 												// Demande du nom
+		Nom = sc.next();																		// Lecture du nom grâce au scanner					
+		System.out.println(" Quel est votre prénom?");											// Demande du nom
+		Prénom = sc.next();																		// Lecture du prénom grâce au scanner
+		System.out.println("Quel est votre département?");										// Demande du département
+		Département = sc.nextInt();																// Lecture du département
 		
-		System.out.println("L'adresse que vous préférez est : "+Adresse1+" ou "+Adresse2+" ?");
+		Adresse1 = Nom+Département+Prénom+"@arfp.asso.fr";										// Création de l'adresse mail 1
+		Adresse2 = Prénom+"."+Nom+Département+"@arfp.asso.fr";									// Création de l'adresse mail 2
 		
+		System.out.println("L'adresse que vous préférez est : "+Adresse1+" ou "+Adresse2+" ?");	// Proposition des adresses mail
+		
+		// Fermeture du scanner
 		sc.close();
 	}
 
