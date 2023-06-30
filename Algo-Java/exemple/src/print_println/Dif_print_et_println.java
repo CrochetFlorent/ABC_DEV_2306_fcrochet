ff4756f73e0bfa3607e77404671e1138c2b353c2
CrochetFlorent <crochet.florent@gmail.com>
2023-06-30 11:18:17 +0200

    Update

---
+++ Algo-Java/exemple/src/print_println/Dif_print_et_println.java
@@ -0,0 +1,15 @@
+package print_println;
+
+public class Dif_print_et_println {
+
+	public static void main(String[] args) {
+
+		int faces = 6;
+		System.out.print (faces);
+		System.out.print ('.');
+		System.out.println (faces);
+		System.out.print ('.');
+
+	}
+
+}
