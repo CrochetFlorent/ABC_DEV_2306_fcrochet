9fb04d57a765d94daf1cee939c259ba6705f0dd4
CrochetFlorent <crochet.florent@gmail.com>
2023-07-04 11:30:26 +0200

    Mise a jour

--- Algo-Java/ExercicesJava/src/Exercice2_3/App.java
+++ Algo-Java/ExercicesJava/src/Exercice2_3/App.java
@@ -10,9 +10,14 @@ public class App {
 		double a;
 		double b;
 		double c;
+		int choix;
+		boolean end;
 		
 		Scanner sc = new Scanner(System.in);
-
+		end = true;
+		
+		do {
+			
 		System.out.println("Entrer une nombre entier");
 		a = sc.nextInt();
 		System.out.println("Entrez un deuxième nombre entier");
@@ -20,24 +25,43 @@ public class App {
 		System.out.println("Entrez un troisième nombre entier");
 		c = sc.nextInt();
 		
-		/*if (a<b && a<c) {
-			if(b<c) {
-				System.out.println(a+"<"+b+"<"+c);
-			}else {
-				System.out.println(a+"<"+c+"<"+b);}
+		if (a < b &&  b < c)
+		{
+			System.out.println(a+" < "+b+" < "+c);
 		}
-		if (b<c && b<a) {
-			if(a<c) {
-				System.out.println(b+"<"+a+"<"+c);
-			}else {
-				System.out.println(b+"<"+c+"<"+a);}
-		}*/
+		if (a < c &&  c < b)
+		{
+			System.out.println(a+" < "+c+" < "+b);
+		}
+		if (b < a &&  a < c)
+		{
+			System.out.println(b+" < "+a+" < "+c);
+		}
+		if (b < c &&  c < a)
+		{
+			System.out.println(b+" < "+c+" < "+a);
+		}
+		if (c < b &&  b < a)
+		{
+			System.out.println(c+" < "+b+" < "+a);
+		}
+		if (c < a &&  a < b)
+		{
+			System.out.println(b+" < "+a+" < "+c);
+		}
 		
-		while (a>=c)
-		{		
-				
+		System.out.println("Voulez vous continuer? 1 pour oui 2 pour non");
+		choix = sc.nextInt();
+		
+		if (choix == 2)
+		{
+			end = false;
 		}
-
+		
+		}while (end == true);
+		
+		sc.close();
+		
 	}
 
 }
