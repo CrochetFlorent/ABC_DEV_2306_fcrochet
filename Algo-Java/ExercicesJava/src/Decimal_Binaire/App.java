68e7f6a3c4746dba36a1daec2ec82e1751186b88
CrochetFlorent <crochet.florent@gmail.com>
2023-07-21 08:35:32 +0200

    Commit Rajout correction et orga + pseudo code convertisseur décimal binaire

--- Algo-Java/ExercicesJava/src/Decimal_Binaire/App.java
+++ Algo-Java/ExercicesJava/src/Decimal_Binaire/App.java
@@ -8,12 +8,42 @@ public class App {
 
 	public static void main(String[] args) {		
 		
+		/*Variables
+		 * 		base10 est un entier
+		 * 		end est un entier
+		 * 		Création d'une liste de nom list
+		 * 
+		 * Debut du Programme
+		 * 		Ecrire "Entrer le nomnre entier en base 10 pour le convertir en base 2
+		 * 		Lire base10
+		 * 		end = base10
+		 * 
+		 * 		Tant que base10 > 1
+		 * 			Si base10 est divisible par 2 alors			// Ici on traite le cas ou le reste vaut 0
+		 * 				on ajoute un 0 à la liste
+		 *			Sinon 
+		 *				on ajoute un 1 à la liste				// Ici on traite le cas ou le reste vaut 1
+		 *			Fin si
+		 *		Fin tant que
+		 *		Si base10 n'est pas divisible par 2 alors     // Ici on traite le cas ou il reste 1 à la division 
+		 *			on ajoute un 1 à la liste
+		 *		Fin si
+		 *
+		 *		On inverse la liste								// Pour lire les 0 et 1 dans le bon sens
+		 *
+		 *		Ecrire " La base 2 de ",end," est "				
+		 *
+		 *		Pour i de 0 à la taille de la liste faire
+		 *			Ecrire list(i)
+		 *		Fin pour
+		 *	Fin du Programme
+		 */
 		int base10,end;
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Entrer un nombre entier en base 10 pour le convertir en base 2");
+		System.out.println("Entrer un nombre entier en base 10 pour le convertir en base 2.");
 		base10 = sc.nextInt();
 		end = base10;
 		
