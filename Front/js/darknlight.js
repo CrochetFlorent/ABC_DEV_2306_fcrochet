9ddfb7d002a5ff2cbb1d5d15ee561ebf2b8eeef0
CrochetFlorent <137294704+CrochetFlorent@users.noreply.github.com>
2023-10-25 18:14:19 +0200

    Commit Dark n Light

---
+++ Front/js/darknlight.js
@@ -0,0 +1,11 @@
+var light = document.getElementsByName('light');
+var dark = document.getElementsByName('dight');
+
+function darknlight(dark,light) {
+    if (dark.matches) { // If media query matches
+      light.matches;
+    } else if(light.matches){
+      dark.matches;
+    }
+  }
+
