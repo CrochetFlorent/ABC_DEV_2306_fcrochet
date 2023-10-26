150ce62fffd121ff40de896b23d5fe84a93d638b
CrochetFlorent <137294704+CrochetFlorent@users.noreply.github.com>
2023-10-26 11:33:52 +0200

    Commit fin tp HTML/CSS/JS

--- Front/js/darknlight.js
+++ Front/js/darknlight.js
@@ -1,11 +1,13 @@
-var light = document.getElementsByName('light');
-var dark = document.getElementsByName('dight');
 
-function darknlight(dark,light) {
-    if (dark.matches) { // If media query matches
-      light.matches;
-    } else if(light.matches){
-      dark.matches;
-    }
-  }
+let dark = document.getElementById('theme-btn');
+
+
+dark.addEventListener("click",Dark);
+
+var element = document.querySelector("body").classList;
+
+function Dark() {
+    element.toggle("theme2");
+}
+
 
