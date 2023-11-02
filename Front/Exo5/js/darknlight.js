196688648819b4beef0083b6f3512fc668868007
CrochetFlorent <137294704+CrochetFlorent@users.noreply.github.com>
2023-11-02 15:55:33 +0100

    Commit du soir

--- Front/Exo5/js/darknlight.js
+++ Front/Exo5/js/darknlight.js
@@ -1,14 +1,33 @@
 
-let dark = document.getElementById('theme-btn');
+const change = document.getElementById('theme-change');
+
+change.addEventListener("click", changeTheme);
+
+const element = document.querySelector("body").classList;
+let darkModeWindows = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+var darkMode= false;
+
+/*if(darkModeWindows)
+{
+    element.toggle('dark');
+    change.innerHTML = darkMode? "Dark" : "Light";
+}else
+{
+    element.toggle(null);
+    change.innerHTML = darkMode? "Light" : "Dark";
+}*/
 
 
-dark.addEventListener("click",Dark);
-
-let element = document.querySelector("body").classList;
-let darklight = "false";
-
-function Dark() {
-    element.toggle("theme2");
+function changeTheme(){
+    change.innerHTML = darkMode? "Dark" : "Light";
+    if(!darkMode){
+        element.toggle('dark');
+        darkMode = true;    
+    }else{
+        element.remove('dark');
+        element.toggle(null);
+        darkMode = false;
+    }
 }
-
 
