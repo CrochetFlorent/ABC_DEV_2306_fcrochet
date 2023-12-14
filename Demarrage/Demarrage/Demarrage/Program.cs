0c8c2590ad14978489dd7258ca71205aa9eca3dc
CrochetFlorent <137294704+CrochetFlorent@users.noreply.github.com>
2023-12-14 15:56:03 +0100

    Commit du soir

---
+++ Demarrage/Demarrage/Demarrage/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demarrage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("J'affiche i pour la première fois.");
+                }
+                else
+                {
+                    Console.WriteLine("J'affiche i pour la " +(i + 1)+ "ieme fois.");
+                }
+                
+            }
+        }
+    }
+}
