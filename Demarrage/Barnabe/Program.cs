1e42545d4771befb73b301a139c5956633a8bbc5
CrochetFlorent <137294704+CrochetFlorent@users.noreply.github.com>
2024-01-08 10:12:35 +0100

    Commit Sophie

---
+++ Demarrage/Barnabe/Program.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+
+namespace Barnabe
+{
+    public class testMock
+    {
+        exercice exo;
+        public testMock(exercice _exo)
+        {
+            this.exo = _exo;
+        }
+    }
+
+    public class exercice
+    {
+        public double Barnabe(double somme)
+        {
+            int nbMagasins = 0;
+            if (somme > 1)
+            {
+                while (somme / 2 - 1 >= 2)
+                {
+                    somme = somme / 2 - 1;
+                    nbMagasins += 1;
+                }
+            }
+            else if (somme > 1 && somme < 2)
+            {
+                somme = 0;
+                nbMagasins += 1;
+            }
+
+            return nbMagasins;
+        }
+    }
+    internal class Program
+    {   
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var barnabeMock = new Mock<exercice>();
+            barnabeMock.Setup(b => b.Barnabe(100));
+
+            testMock test = new testMock(barnabeMock.Object);
+            test.Equals(100);
+
+            barnabeMock.Verify(v => v.Barnabe(It.IsAny<int>()));
+
+        }
+    }
+}
