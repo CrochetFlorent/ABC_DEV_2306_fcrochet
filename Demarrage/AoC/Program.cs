108cdc428e43bbef91c1ba4e2e873d811c89fdfe
CrochetFlorent <crochet.florent@gmail.com>
2024-01-22 15:35:27 +0100

    Mise a jour

---
+++ Demarrage/AoC/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AoC
+{
+    internal class Program
+    {
+        public static void aoc01()
+        {
+            StreamReader sr = new StreamReader("C:\\input01.txt");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Demarrage/TestFibonacci/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestFibonacci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
